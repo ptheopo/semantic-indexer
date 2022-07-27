170c23d1e6b1a9b3cfbc8a5e9756eb658433c660
tpoac <t.poac@cyim.com>
2022-07-27 18:04:27 +0200

    Refacto

--- APP/app.js
+++ APP/app.js
@@ -5,13 +5,9 @@ const path = require('path');
 
 app.use(cors());
 
-app.use(
-  express.static(path.join(__dirname, "node_modules/bootstrap/dist/"))
-);
+app.use(express.static(path.join(__dirname, "node_modules")));
+app.use(express.static(path.join(__dirname, "static")));
 
-/* Currently not an app, tmp */
-
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
